d98f127e457ff12f41a1196b86e8dc8cd5060f13
Marcos Tenrero <tenrero@aol.com>
2018-03-17 20:56:41 +0100

    Increased code coverage in API Controller

--- api_controller_test.go
+++ api_controller_test.go
@@ -5,6 +5,7 @@ import (
 	"net/http/httptest"
 	"net/url"
 	"testing"
+	"time"
 
 	"github.com/sirupsen/logrus"
 
@@ -63,4 +64,59 @@ func TestCSV(t *testing.T) {
 	engine.ServeHTTP(recorder, req)
 
 	assert.Equal(t, 200, recorder.Code)
+}
+
+func TestGetContainerNotFound(t *testing.T) {
+	registry := serviceDiscovery.NewRegistryCollection()
+
+	atqContext := ATQContext{registry: registry}
+
+	engine := controllerNetworkHandler(&atqContext, logrus.New())
+
+	recorder := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", apiVersion+"/container/NOTFOUND", nil)
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	engine.ServeHTTP(recorder, req)
+
+	assert.Equal(t, 404, recorder.Code)
+}
+
+func TestGetContainerOK(t *testing.T) {
+	registry := serviceDiscovery.NewRegistryCollection()
+	registryEntry := serviceDiscovery.MakeRegistryEntry(time.Now().Unix(), "10.0.0.1", "testinghost", "TESTING", 2)
+	registry, _ = registry.Add(registryEntry)
+
+	atqContext := ATQContext{registry: registry}
+
+	engine := controllerNetworkHandler(&atqContext, logrus.New())
+
+	recorder := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", apiVersion+"/container/testinghost", nil)
+	engine.ServeHTTP(recorder, req)
+
+	assert.Equal(t, 200, recorder.Code)
+}
+
+func TestRegisterContainerAlreadyRegistered(t *testing.T) {
+	registry := serviceDiscovery.NewRegistryCollection()
+	registryEntry := serviceDiscovery.MakeRegistryEntry(time.Now().Unix(), "10.0.0.1", "testinghost", "TESTING", 2)
+	registry, _ = registry.Add(registryEntry)
+
+	atqContext := ATQContext{registry: registry}
+
+	engine := controllerNetworkHandler(&atqContext, logrus.New())
+
+	form := url.Values{}
+
+	form.Add("containerIP", "10.0.0.1")
+	form.Add("hostname", "testinghost")
+	form.Add("group", "TESTING")
+
+	recorder := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", apiVersion+"/container", nil)
+	req.PostForm = form
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	engine.ServeHTTP(recorder, req)
+
+	assert.Equal(t, 409, recorder.Code)
 }
